da46e791221080d47ac29591bea89dbfad1dd154
Daniel Georges <dannnn.georges@gmail.com>
2020-07-29 21:02:35 -0300

    Update VideocarContainer
    The name of the element appears onMouseOver.

--- src/Components/Carousel/components/VideoCard/index.js
+++ src/Components/Carousel/components/VideoCard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VideoCardContainer } from './styles';
+import { VideoCardContainer, VideoCardTitle } from './styles';
 
 function getYouTubeId(youtubeURL) {
   return youtubeURL
@@ -19,8 +19,9 @@ function VideoCard({ videoTitle, videoURL, categoryColor }) {
       target="_blank"
       style={{ borderColor: categoryColor || 'red' }}
       title={videoTitle}
-    />
+    >
+      <VideoCardTitle>{videoTitle}</VideoCardTitle>
+    </VideoCardContainer>
   );
 }
-
 export default VideoCard;
--- src/Components/Carousel/components/VideoCard/styles.js
+++ src/Components/Carousel/components/VideoCard/styles.js
@@ -5,13 +5,24 @@ export const VideoCardContainer = styled.a`
   width: 298px;
   border: 2px solid;
   display: inline-block;
+  text-decoration: none;
   background-image: ${({url}) => `url(${url})`};
   background-size: cover;
   background-position: center;
   border-radius: 10px;
   transition: opacity .3s;
+  transition: font-size .3s;
+  font-size: 0px;
+  text-justify:bottom;
   &:hover,
   &focus {
     opacity: .5;
+    font-size: 14px;
   }
+`;
+
+export const VideoCardTitle = styled.h2`
+  font-weight:normal;
+  margin-top: 160px;
+  opacity: 0.9;
 `;
